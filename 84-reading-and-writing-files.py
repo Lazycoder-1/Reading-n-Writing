4c45cd7e1a780e0a1eb74669db01126a651c6315
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-07-17 19:20:00 +0000

    Add files via upload

---
+++ 84-reading-and-writing-files.py
@@ -0,0 +1,13 @@
+# reading and writing files in python
+
+file = open('points.txt','r')
+file.read()
+file.close()
+print(file)
+
+# files = open('points.txt','w')
+# files.write('hi from the other side')
+# files.close()
+# print(files)
+
+
